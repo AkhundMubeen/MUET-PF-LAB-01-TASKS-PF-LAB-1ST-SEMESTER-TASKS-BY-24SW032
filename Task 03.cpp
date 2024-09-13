e224a37b061af11c8a253832b7ec49a7ca78879e
AkhundMubeen <akhundmubeen07@gmail.com>
2024-09-13 23:11:38 +0500

    Task 03.cpp
    PF Lab Task#03: 
    =>Write a program in C++ to find size of fundamental Data Types!
    
    Task Given By: Respected SIR SAJJAD ALI.
    Task Done By: Mubeen Akhund
    Roll NO: 24SW032

---
+++ Task 03.cpp
@@ -0,0 +1,21 @@
+/*  PF Lab Task # 03 
+    Write a program in C++ to find size of fundamental data types. */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	cout<<"The Size Of Fundamental Data Types Is Given:"<<endl<<endl;
+	
+	cout<<"The Size Of Integer (int) Data Type Is: "<<sizeof(int)<<" Bytes\n";
+	cout<<"The Size Of Float (float) Data Type Is: "<<sizeof(float)<<" Bytes\n";
+	cout<<"The Size Of Character (char) Data Type Is: "<<sizeof(char)<<" Byte\n";
+	cout<<"The Size Of Double (double) Data Type Is: "<<sizeof(double)<<" Bytes\n";
+	cout<<"The Size Of Boolean (bool) Data Type Is: "<<sizeof(bool)<<" Bytes\n";
+	cout<<"The Size Of Text (string) Data Type Is: "<<sizeof(string)<<" Bytes\n";
+	return 0;
+	
+}
+
+//Task done by Mubeen Akhund 24SW032; MUET.
