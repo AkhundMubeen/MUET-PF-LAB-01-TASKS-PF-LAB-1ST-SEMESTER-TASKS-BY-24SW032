8d950977ecfa9f3dffdb8cce893f3baa6656b5f2
AkhundMubeen <akhundmubeen07@gmail.com>
2024-09-13 21:43:44 +0500

    TASK 01.cpp
    PF Lab Task 01: 
    
    =>    Write a C++ Program to print the following lines:
    	 You are 10 years old.
    	 You are too young to play the game.
             (Hint: use an int variable for age)
    
    Task given by: Respected SIR SAJJAD ALI.
    Done by: Mubeen Akhund.
    Roll No: 24SW032

---
+++ TASK 01.cpp
@@ -0,0 +1,17 @@
+/*   Task # 01
+     Write a C++ program to print the following lines:
+	 You are 10 years old.
+	 You are too young to play the game (Hint: use an int variable for age). */
+ 
+#include<iostream>
+using namespace std;
+int main()
+{
+  int age;
+  age=10;
+  cout<<"You are "<<age<<" years old.\n";
+  cout<<"You are too young to play the game.";
+  return 0;
+}
+
+//Task done by Mubeen Akhund 24SW032; MUET.
