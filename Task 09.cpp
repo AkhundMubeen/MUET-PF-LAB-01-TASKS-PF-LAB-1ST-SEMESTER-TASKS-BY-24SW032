fd0df259342b32f5729c07e8635c523cef57c1c7
AkhundMubeen <akhundmubeen07@gmail.com>
2024-09-19 20:09:08 +0500

    Task 09.cpp
    TASK#09:
    PF LAB TASK:
    INSERT A NEW LINE AFTER "HELLO WORLD", BY USING A SPECIAL CHARACTER.
    
    Task given by: Respected SIR SAJJAD ALI.
    Done by: Mubeen Akhund.
    Roll No: 24SW032

---
+++ Task 09.cpp
@@ -0,0 +1,20 @@
+/*
+   TASK#09:
+   INSERT A NEW LINE AFTER "HELLO WORLD", BY USING A SPECIAL CHARACTER.
+                                                                         */
+				
+																		 
+   #include<iostream>
+   using namespace std;
+   int main()
+   {
+   	cout<<"Hello World\n";
+   	
+	   /* The special character used to separate or insert a new line is basically
+	      an ESCAPE SEQUENCE OF "\n".
+	   "\n" basically separates or inserts a new line after an existing line! */
+	   
+	return 0;
+   }
+   
+   // Task done by Mubeen Akhund 24SW032; MUET.
