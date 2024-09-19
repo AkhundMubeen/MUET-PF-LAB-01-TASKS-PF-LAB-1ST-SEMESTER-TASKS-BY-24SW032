7eb313ec3b2d52d016137f6e9bcbc45ea00a21d3
AkhundMubeen <akhundmubeen07@gmail.com>
2024-09-19 19:38:52 +0500

    Task 06.cpp
    TASK#06
    PF LAB TASK:
       Write three C++ statements to print the asterisk pattern as 
       shown below:
    	
    	*****
    	*****
    	*****
    
    Task given by: Respected SIR SAJJAD ALI.
    Done by: Mubeen Akhund.
    Roll No: 24SW032

---
+++ Task 06.cpp
@@ -0,0 +1,22 @@
+/* Task#06: 
+   Write three C++ statements to print the asterisk pattern as 
+   shown below:
+	
+	*****
+	*****
+	*****    
+	
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"***** \n";
+	cout<<"***** \n";
+	cout<<"***** \n";
+	
+	return 0;
+}
+
+// Task done by Mubeen Akhund 24SW032; MUET.
