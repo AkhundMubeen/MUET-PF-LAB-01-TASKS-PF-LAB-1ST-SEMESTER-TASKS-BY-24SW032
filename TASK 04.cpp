d2678b249858603a7560ba5058a2c577432278ff
AkhundMubeen <akhundmubeen07@gmail.com>
2024-09-19 19:07:38 +0500

    TASK 04.cpp
    Task: 04:
    PF LAB TASK:
    Write a C++ Program to declare an Integer Variable Hours And Assign it a value of your choice and convert Hours in terms of Minutes.
    
    Task given by: Respected SIR SAJJAD ALI.
    Done by: Mubeen Akhund.
    Roll No: 24SW032

---
+++ TASK 04.cpp
@@ -0,0 +1,17 @@
+/* Task#04:
+   Write a C++ program to declare an integer variable Hours and
+   assign it a value of your choice and convert Hours in terms of Minutes. */
+   
+#include<iostream>
+using namespace std;
+int main()
+{
+	int Hours;
+	cout<<"Enter The Number Of Hours: \n";
+	cin>>Hours;
+	cout<<Hours<<" Hours In Terms Of Minutes Will Be: \n";
+	cout<<Hours*60<<" Minutes \n";
+	return 0;
+}
+
+//Task done by Mubeen Akhund 24SW032; MUET.
