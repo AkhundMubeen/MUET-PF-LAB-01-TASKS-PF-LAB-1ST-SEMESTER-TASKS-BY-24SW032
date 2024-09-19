92a3dd62511c8666c917832ba57b9f9317e9c4e3
AkhundMubeen <akhundmubeen07@gmail.com>
2024-09-19 19:30:44 +0500

    Task 05.cpp
    TASK#05:
    PF LAB TASK:
    Write a C++ program to declare two integers, one float variable and assign 10, 15, and 12.6 to them respectively. 
    It then prints these values on the screen.
    
    Task given by: Respected SIR SAJJAD ALI.
    Done by: Mubeen Akhund.
    Roll No: 24SW032

---
+++ Task 05.cpp
@@ -0,0 +1,28 @@
+/* Task#05:
+
+   Write a C++ program to declare two integers, one float 
+   variable and assign 10, 15, and 12.6 to them respectively. 
+   It then prints these values on the screen.  */
+   
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	int y;
+	float z;
+	
+	x = 10;
+	y = 15;
+	z = 12.6;
+	
+	cout<<"First Integer Variable: \n"<<x<<endl;
+    
+	cout<<"Second Integer Variable: \n"<<y<<endl;
+	
+	cout<<"First Float Variable: \n"<<z<<endl;
+	
+	return 0;
+}
+
+// Task done by Mubeen Akhund 24SW032; MUET.
