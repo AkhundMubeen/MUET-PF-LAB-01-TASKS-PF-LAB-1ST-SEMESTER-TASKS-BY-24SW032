48fd5c52e4816677f3d72f4b2cc73267e6123de8
AkhundMubeen <akhundmubeen07@gmail.com>
2024-09-19 19:52:36 +0500

    Task 08.cpp
    TASK#08:
    Insert the missing part of the code below to output "Hello World!"
    
        CODE:
        int main()
        {
        ________<<"Hello World!";
        return 0;
        }
    
    Task given by: Respected SIR SAJJAD ALI.
    Done by: Mubeen Akhund.
    Roll No: 24SW032

---
+++ Task 08.cpp
@@ -0,0 +1,25 @@
+/*  TASK#08:
+
+    Insert the missing part of the code below to output "Hello World!"
+    
+    CODE:
+    int main()
+   	{
+    ________<<"Hello World!";
+    return 0;
+    }
+    
+*/   
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello World!"<<endl<<endl;
+	
+	cout<<"The Missing Part In The Given Code Of The Task Was 'cout' itself. ";
+	
+	return 0;
+}
+
+// Task done by Mubeen Akhund 24SW032; MUET.
