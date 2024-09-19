ce8bb6863644cbda1dcfa122d37bff3c9b6914fa
AkhundMubeen <akhundmubeen07@gmail.com>
2024-09-19 21:15:04 +0500

    Task 11.cpp
    TASK#11
    PF LAB TASK:
    
    FILL IN THE MISSING PARTS TO CREATE THREE VARIABLES OF SAME DATA TYPE, USING A COMMA-SEPARATED LIST.
    
    CODE:
    ________ x=5 ___________ y=6 __________ z=50;
    cout<<x+y+z;
    
    
    Task given by: Respected SIR SAJJAD ALI.
    Done by: Mubeen Akhund.
    Roll No: 24SW032

---
+++ Task 11.cpp
@@ -0,0 +1,19 @@
+/*
+TASK#11:
+FILL IN THE MISSING PARTS TO CREATE THREE VARIABLES OF SAME DATA TYPE, USING A COMMA-SEPARATED LIST.
+________ x=5 ___________ y=6 __________ z=50;
+cout<<x+y+z;
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=5, y=6, z=50;
+	cout<<x+y+z;
+	
+	return 0;
+}
+
+
+//Task done by Mubeen Akhund 24SW032; MUET.
