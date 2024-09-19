799bc6cb88758a939e6f9041b8a9abf35e756970
AkhundMubeen <akhundmubeen07@gmail.com>
2024-09-19 21:37:25 +0500

    Task 12.cpp
    TASK#12:
    PF LAB TASK:
    
    CREATE A VARIABLE CALLED Z, ASSIGN X=Y TO IT, AND DISPLAY THE RESULT:
    
    CODE:
    
    int x=5;
    int y=10;
    _____=x+y;
    
    cout<<_______ ;
    
    
    
    
    Task given by: Respected SIR SAJJAD ALI.
    Done by: Mubeen Akhund.
    Roll No: 24SW032

---
+++ Task 12.cpp
@@ -0,0 +1,29 @@
+/* 
+PF LAB TASK#12:
+CREATE A VARIABLE CALLED Z, ASSIGN X=Y TO IT, AND DISPLAY THE RESULT:
+
+CODE:
+
+int x=5;
+int y=10;
+_____=x+y;
+
+cout<<_______ ;
+*/
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=5;
+	int y=10;
+	int z=x+y;
+	
+	cout<<z<<endl;
+	
+	return 0;
+}
+
+
+//Task done by Mubeen Akhund 24SW032; MUET.
