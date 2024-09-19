f28bfee7f653ef5859a558a0997536ad77f963ba
AkhundMubeen <akhundmubeen07@gmail.com>
2024-09-19 20:50:02 +0500

    Task 10.cpp
    TASK#10
    PF LAB TASK:
    COMMENTS IN C++ ARE WRITTEN WITH SPECIAL CHARACTERS.
    INSERT THE MISSING PARTS:
    
    1) ________ "//" THIS IS A SINGLE LINE COMMENT
    2) ________ "*/" THIS IS A MULTI LINE COMMENT
    
    Task given by: Respected SIR SAJJAD ALI.
    Done by: Mubeen Akhund.
    Roll No: 24SW032

---
+++ Task 10.cpp
@@ -0,0 +1,39 @@
+/* 
+TASK#10:
+
+COMMENTS IN C++ ARE WRITTEN WITH SPECIAL CHARACTERS.
+INSERT THE MISSING PARTS:
+
+1) ________ "/ /" THIS IS A SINGLE LINE COMMENT
+2) ________ "* /" THIS IS A MULTI LINE COMMENT
+*/
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"The USES Of '//' and '*/' ";
+
+
+
+	
+	// This is a Single Line Comment and is written with special character: "//" called as "Double Forward Slashes".
+	
+	
+	
+	
+	/* 
+	
+	This is a Multi Line Comment.
+	Multi line comments are written with special characters:
+	1) "/ *" This "First (Forward Slash) and then (Asterik)" without Spaces' Combination is used to Start or Initialise the Multi Line Comment.
+	2) "* /" This "First (Asterik) and then (Forward Slash)" without Spaces' Combination is used to Finalize or Terminate the Multi Line Comment.
+	
+	*/
+	
+	return 0;
+}
+
+
+//Task done by Mubeen Akhund 24SW032; MUET.
